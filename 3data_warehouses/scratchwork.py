20befd8f3db680103b3670e7715a8b4890e464b3
Chris Seal <chriseal@gmail.com>
2020-02-10 14:57:29 -0500

    proj3

--- 3data_warehouses/scratchwork.py
+++ 3data_warehouses/scratchwork.py
@@ -1,4 +1,20 @@
+staging_songs_copy = ("""
+    COPY staging_songs
+    FROM {}
+    CREDENTIALS 'aws_iam_role={}'
+    COMPUPDATE off
+    REGION 'us-west-2'
+    FORMAT AS JSON 'auto';
+""").format(SONG_DATA, IAM_ARN)
 
+
+KEY                    = config.get('AWS','KEY')
+SECRET                 = config.get('AWS','SECRET')
+HOST                 = config.get('CLUSTER','HOST')
+IAM_ARN                 = config.get('IAM_ROLE','ARN')
+LOG_DATA                 = config.get('S3','LOG_DATA')
+LOG_JSONPATH                 = config.get('S3','LOG_JSONPATH')
+SONG_DATA                 = config.get('S3','SONG_DATA')
 
 
 
