c47f126ad28a44892c54edaae0d48aa9c8953410
Chris Seal <chriseal@gmail.com>
2020-03-01 20:15:46 -0500

    airflow l2

--- 5data_pipelines_w_airflow/lesson1.py
+++ 5data_pipelines_w_airflow/lesson1.py
@@ -57,3 +57,231 @@ task = PythonOperator(
         task_id="hello_world_task",
         python_callable=hello_world,
         dag=dag)
+
+
+# lesson1.exercise3
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+def hello_world():
+    logging.info("Hello World")
+
+
+def addition():
+    logging.info(f"2 + 2 = {2+2}")
+
+
+def subtraction():
+    logging.info(f"6 -2 = {6-2}")
+
+
+def division():
+    logging.info(f"10 / 2 = {int(10/2)}")
+
+
+dag = DAG(
+    "lesson1.solution3",
+    schedule_interval='@hourly',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1))
+
+hello_world_task = PythonOperator(
+    task_id="hello_world",
+    python_callable=hello_world,
+    dag=dag)
+
+addition_task = PythonOperator(
+    task_id="addition",
+    python_callable=addition,
+    dag=dag)
+
+subtraction_task = PythonOperator(
+    task_id="subtraction",
+    python_callable=subtraction,
+    dag=dag)
+
+division_task = PythonOperator(
+    task_id="division",
+    python_callable=division,
+    dag=dag)
+
+# Configure Task Dependencies
+hello_world_task >> addition_task
+hello_world_task >> subtraction_task
+subtraction_task >> division_task
+addition_task >> division_task
+
+
+# lesson1.exercise4
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+
+#
+# TODO: There is no code to modify in this exercise. We're going to create a connection and a
+# variable.
+# 1. Open your browser to localhost:8080 and open Admin->Variables
+# 2. Click "Create"
+# 3. Set "Key" equal to "s3_bucket" and set "Val" equal to "udacity-dend"
+# 4. Set "Key" equal to "s3_prefix" and set "Val" equal to "data-pipelines"
+# 5. Click save
+# 6. Open Admin->Connections
+# 7. Click "Create"
+# 8. Set "Conn Id" to "aws_credentials", "Conn Type" to "Amazon Web Services"
+# 9. Set "Login" to your aws_access_key_id and "Password" to your aws_secret_key
+# 10. Click save
+# 11. Run the DAG
+
+def list_keys():
+    hook = S3Hook(aws_conn_id='aws_credentials')
+    bucket = Variable.get('s3_bucket')
+    prefix = Variable.get('s3_prefix')
+    logging.info(f"Listing Keys from {bucket}/{prefix}")
+    keys = hook.list_keys(bucket, prefix=prefix)
+    for key in keys:
+        logging.info(f"- s3://{bucket}/{key}")
+
+
+dag = DAG(
+        'lesson1.exercise4',
+        start_date=datetime.datetime.now())
+
+list_task = PythonOperator(
+    task_id="list_keys",
+    python_callable=list_keys,
+    dag=dag
+)
+# Solution 4: Connections and Hooks
+# Below is the solution for Exercise 4: Connections and Hooks.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+
+
+def list_keys():
+    hook = S3Hook(aws_conn_id='aws_credentials')
+    bucket = Variable.get('s3_bucket')
+    prefix = Variable.get('s3_prefix')
+    logging.info(f"Listing Keys from {bucket}/{prefix}")
+    keys = hook.list_keys(bucket, prefix=prefix)
+    for key in keys:
+        logging.info(f"- s3://{bucket}/{key}")
+
+
+dag = DAG(
+        'lesson1.exercise4',
+        start_date=datetime.datetime.now())
+
+list_task = PythonOperator(
+    task_id="list_keys",
+    python_callable=list_keys,
+    dag=dag
+)
+
+
+# Instructions
+# Use the Airflow context in the pythonoperator to complete the TODOs below. Once you are done, run your DAG and check the logs to see the context in use.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+
+
+def log_details(*args, **kwargs):
+    #
+    # TODO: Extract ds, run_id, prev_ds, and next_ds from the kwargs, and log them
+    # NOTE: Look here for context variables passed in on kwargs:
+    #       https://airflow.apache.org/macros.html
+    # The link for the Airflow documentation on context variables has changed since the video was created. Here is the new link: https://airflow.apache.org/macros.html
+    #
+    ds = kwargs['ds']
+    run_id = kwargs['run_id']
+    previous_ds = kwargs.get('prev_ds')
+    next_ds = kwargs.get('next_ds')
+
+    logging.info(f"Execution date is {ds}")
+    logging.info(f"My run id is {run_id}")
+    if previous_ds:
+        logging.info(f"My previous run was on {previous_ds}")
+    if next_ds:
+        logging.info(f"My next run will be {next_ds}")
+
+dag = DAG(
+    'lesson1.exercise5',
+    schedule_interval="@daily",
+    start_date=datetime.datetime.now() - datetime.timedelta(days=2)
+)
+
+list_task = PythonOperator(
+    task_id="log_details",
+    python_callable=log_details,
+    provide_context=True,
+    dag=dag
+)
+
+
+# lesson1.solution6
+# Similar to what you saw in the demo, copy and populate the trips table. Then, add another operator which creates a traffic analysis table from the trips table you created. Note, in this class, we won’t be writing SQL -- all of the SQL statements we run against Redshift are predefined and included in your lesson.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    redshift_hook.run(sql_statements.COPY_ALL_TRIPS_SQL.format(credentials.access_key, credentials.secret_key))
+
+
+dag = DAG(
+    'lesson1.solution6',
+    start_date=datetime.datetime.now()
+)
+
+create_table = PostgresOperator(
+    task_id="create_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_task = PythonOperator(
+    task_id='load_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_data_to_redshift
+)
+
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.LOCATION_TRAFFIC_SQL
+)
+
+create_table >> copy_task
+copy_task >> location_traffic_task
